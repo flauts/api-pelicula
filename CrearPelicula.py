e635c39eb2ba911916dc7bb87e98d62d5703d79f
flauts <124704878+flauts@users.noreply.github.com>
2024-11-07 08:46:20 -0500

    Add files via upload

--- CrearPelicula.py
+++ CrearPelicula.py
@@ -5,9 +5,21 @@ import os
 def lambda_handler(event, context):
     # Entrada (json)
     print(event) # Log json en CloudWatch
-    tenant_id = event['body']['tenant_id']
-    pelicula_datos = event['body']['pelicula_datos']
-    nombre_tabla = os.environ["TABLE_NAME"]
+    try:
+        tenant_id = event['body']['tenant_id']
+        pelicula_datos = event['body']['pelicula_datos']
+        nombre_tabla = os.environ["TABLE_NAME"]
+    except KeyError as e:
+        error = { 
+            "tipo" : "ERROR",
+            "log_datos": event["body"]
+        }
+        print(error)
+        return {
+            'statusCode': 400,
+            'error': f"Error: {str(e)}",
+            'message': "tenant_id y pelicula_datos son requeridos"
+        }
     # Proceso
     uuidv4 = str(uuid.uuid4())
     pelicula = {
@@ -19,7 +31,12 @@ def lambda_handler(event, context):
     table = dynamodb.Table(nombre_tabla)
     response = table.put_item(Item=pelicula)
     # Salida (json)
-    print(pelicula) # Log json en CloudWatch
+    info = {
+        "tipo" : "INFO",
+        "log_datos": pelicula
+    }
+    print(info)
+    # print(pelicula) # Log json en CloudWatch
     return {
         'statusCode': 200,
         'pelicula': pelicula,
